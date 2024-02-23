db5bbb2e291b4877b23deeda19d0a2ad921fc475
Oleksandr <aleksandr.master88@gmail.com>
2024-02-23 15:53:19 +0200

    hw_6 fix

--- js/script.js
+++ js/script.js
@@ -7,11 +7,8 @@ let age = prompt('Скільки Вам років?');
 
 alert(`Дякуєм ${firstName} ${lastName} за реєстрацію (у Вас достатньо років(${age}))!`)
 
-alert('Дякуєм' + ' ' + firstName + ' ' + lastName + ' ' + 'за реєстрацію (у Вас достатньо років('+age+'))!')
-
 
 // Task #2
 let num = 12345;
-let str = '' + num;
 
-alert(str[0]+' '+str[1]+' '+str[2]+' '+str[3]+' '+str[4]);
+alert(num.toString().split('').join(' '));
