00519c52c348c42380c3762e470969b4c03a1987
Oleksandr <aleksandr.master88@gmail.com>
2024-02-23 13:20:28 +0200

    hw_6 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+// Task #1
+let firstName = prompt('Як Вас звати?');
+let lastName = prompt('Як Ваше Прізвище?');
+let age = prompt('Скільки Вам років?');
+
+alert(`Дякуєм ${firstName} ${lastName} за реєстрацію (у Вас достатньо років(${age}))!`)
+
+alert('Дякуєм' + ' ' + firstName + ' ' + lastName + ' ' + 'за реєстрацію (у Вас достатньо років('+age+'))!')
+
+
+// Task #2
+let num = 12345;
+let str = '' + num;
+
+alert(str[0]+' '+str[1]+' '+str[2]+' '+str[3]+' '+str[4]);
