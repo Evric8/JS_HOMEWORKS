2b68ad7e4430ec72341dac60175c59be5d68b8f0
Oleksandr <aleksandr.master88@gmail.com>
2024-02-23 11:52:34 +0200

    hw_7 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// Task #1
+let admin;
+let name;
+
+name = 'John';
+
+admin = name;
+
+alert(admin);
+
+// Task #2
+const planet = 'Earth';
+const firstName = '';
+
+// Task #3
+let str = '10' + 10;
+console.log(typeof str);
