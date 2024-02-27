65e5c75fb14d0fba800b0ae2601813dd09e35712
Oleksandr <aleksandr.master88@gmail.com>
2024-02-27 12:46:24 +0200

    hw_6 fix 27.02

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Task #1
-let firstName = prompt('Як Вас звати?');
-let lastName = prompt('Як Ваше Прізвище?');
-let age = prompt('Скільки Вам років?');
+const firstName = prompt('Як Вас звати?');
+const lastName = prompt('Як Ваше Прізвище?');
+const age = prompt('Скільки Вам років?');
 
 alert(`Дякуєм ${firstName} ${lastName} за реєстрацію (у Вас достатньо років(${age}))!`)
 
 
 // Task #2
-let num = 12345;
+const num = 12345;
 
 alert(num.toString().split('').join(' '));
