d3bf3b80a8a09260253b63952bcfcd82929a98b8
Oleksandr <aleksandr.master88@gmail.com>
2024-02-28 19:28:02 +0200

    hw_9 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+
+const degree = prompt('Введіть кількість градусів.');
+const meaning = prompt('Введіть символ С - якщо ' +
+  'температура в градусах Цельсія, ' +
+  'або F - якщо температура в градусах Фаренгейта');
+let result = null;
+
+if (meaning === 'C') {
+  result = (degree * 1.8) + 32;
+} else {
+  result = (degree - 32) / 1.8;
+}
+
+alert(result);
+
+// ° C = (° F -32) / 1,8        ° F = (° C * 1,8) + 32
