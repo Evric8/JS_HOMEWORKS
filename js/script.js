d0641bddcbb93d7f31825a7ca58db3c8091381ac
Oleksandr <aleksandr.master88@gmail.com>
2024-03-03 15:59:32 +0200

    hw_10 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const one = +prompt('Введіть число.');
+const two = +prompt('Введіть друге число.');
+
+console.group();
+console.log(`Результат: ${one} + ${two} = ` + (one + two));
+console.log(`Результат: ${one} - ${two} = ` + (one - two));
+console.log(`Результат: ${one} * ${two} = ` + (one * two));
+console.log(`Результат: ${one} / ${two} = ` + (one / two));
+console.groupEnd();
