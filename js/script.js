da3e0af05835c79ab202645c909478150575e824
Oleksandr <aleksandr.master88@gmail.com>
2024-03-03 16:44:34 +0200

    hw_11 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const action = prompt('Що Ви хочете зробити, вкажіть щось одне' +
+  ' (add, sub, mult, div)?');
+const one = +prompt('Вкажіть перше число.');
+const two = +prompt('Вкажіть друге число.');
+
+if(action === 'add') {
+  console.log(`${one} + ${two} = `+(one+two));
+} else if (action === 'sub') {
+  console.log(`${one} - ${two} = `+(one - two));
+} else if (action === 'mult') {
+  console.log(`${one} * ${two} = `+(one * two));
+} else if (action === 'div') {
+  console.log(`${one} / ${two} = `+(one / two));
+}
