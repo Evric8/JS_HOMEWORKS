dc56114ec22e75ed33e5f23389094032222a0112
Oleksandr <aleksandr.master88@gmail.com>
2024-03-05 15:13:19 +0200

    hw_16 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 'use strict';
 
+const firstNum = prompt('Введіть перше число.');
+const secondNum = prompt('Введіть друге число.');
+const thirdNum = prompt('Введіть третє число.');
+
+alert(`Середьнє арифметичне число з Ваших трьох введених = 
+${(Number(firstNum) + Number(secondNum) + Number(thirdNum)) / 3}`);
