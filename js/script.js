7fadf293c7231757a7536c89f653a496f462ecdd
Oleksandr <aleksandr.master88@gmail.com>
2024-03-08 11:37:46 +0200

    hw_15 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const active = prompt('Введіть щось одне, ' +
+  'те що Ви хочете зробити' +
+  'add, sub, mult, div');
+const firstNum = +prompt('Введіть перше число!');
+const secondNum = +prompt('Введіть друге число!');
+
+
+if(active === 'add'){
+  console.log(`${firstNum} + ${secondNum} = ${firstNum + secondNum}`);
+} else if (active === 'sub') {
+  console.log(`${firstNum} - ${secondNum} = ${firstNum - secondNum}`);
+} else if (active === 'mult'){
+  console.log(`${firstNum} * ${secondNum} = ${firstNum * secondNum}`);
+} else if (active === 'div'){
+  console.log(`${firstNum} / ${secondNum} = ${firstNum / secondNum}`);
+}
+
