791852077d11122595ff6e5adae4eaec980faf29
Oleksandr <aleksandr.master88@gmail.com>
2024-03-08 11:08:34 +0200

    hw_14 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const firstNum = +prompt('Введіть перше число!');
+const secondNum = +prompt('Введіть друге число!');
+
+alert(
+  `  ${firstNum} + ${secondNum} = ${firstNum + secondNum},
+  ${firstNum} - ${secondNum} = ${firstNum - secondNum},
+  ${firstNum} * ${secondNum} = ${firstNum * secondNum},
+  ${firstNum} / ${secondNum} = ${firstNum / secondNum}`
+)
