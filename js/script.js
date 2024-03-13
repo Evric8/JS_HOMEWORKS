774e334f067f637964db61155faa4a9f06203c9c
Oleksandr <aleksandr.master88@gmail.com>
2024-03-13 16:21:33 +0200

    hw_25 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const num = +prompt('Введіть число');
+
+if(num < 1) {
+  console.log(NaN);
+} else {
+  for (let i = 2; i <= num; i++) {
+    if(num % i === 0) {
+      console.log(i)
+      break;
+    }
+  }
+}
+
+
