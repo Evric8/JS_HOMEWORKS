47b79290dfae78778d4766a469b3388e43e55e4f
Oleksandr <aleksandr.master88@gmail.com>
2024-03-13 15:48:51 +0200

    hw_24 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const arr = [1, 2, 3, 4, 5];
+
+let sumAar = 0;
+let sumSquare = 0;
+
+for (let i = 1; i <= arr.length ; i++) {
+  sumAar += i;
+  sumSquare += Math.pow(i, 2);
+}
+
+console.log(sumAar);
+console.log(sumSquare);
+
