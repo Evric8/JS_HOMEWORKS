fcf681c66851265c45361dc95417d821281ab1d5
Oleksandr <aleksandr.master88@gmail.com>
2024-03-15 19:50:40 +0200

    hw_28 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const num = prompt('Введіть число!');
+const degree = prompt('Введіть ступінь, до якого ' +
+  'це число необхідно піднести !');
+
+function mathDegree(one, two = 1) {
+  if (isNaN(one) || isNaN(two)) {
+    return 'помилка';
+  } else {
+    return Math.pow(one, two);
+  }
+}
+
+const result = mathDegree(num, degree);
+
+alert(result);
