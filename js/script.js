fe4cde36a2e9209e00ff9def7a05567d3cbab21a
Oleksandr <aleksandr.master88@gmail.com>
2024-03-15 17:39:23 +0200

    hw_27 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+function generateKey(length, characters) {
+  let str = '';
+
+  for (let i = 1; i <= length; i++) {
+    str += characters[Math.round(Math.random() * characters.length)];
+  }
+
+  return str;
+}
+
+const key = generateKey(16, characters);
+
+console.log(key);
