6dfcf5c1ad2248c21f31711c8b21ebf1d6898f9d
Oleksandr <aleksandr.master88@gmail.com>
2024-03-17 12:59:25 +0200

    hw_30 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const arr = [1, 2, 3, -1, -2, -3];
+
+const newArr = function (item) {
+  const exampleArr = [];
+
+  if (item.length === 0) {
+    return 'Масив порожній!';
+  }
+
+  for (let i = 0; i < item.length; i++) {
+    if (Math.sign(item[i]) === 1) {
+      exampleArr.push(item[i]);
+    }
+  }
+
+  if (exampleArr.length > 0) {
+    return exampleArr;
+  } else {
+    return null;
+  }
+}
+
+console.log(newArr(arr));
