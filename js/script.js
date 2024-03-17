f4a64c88272ba5288d63750d15e66d6376bc3d58
Oleksandr <aleksandr.master88@gmail.com>
2024-03-17 13:13:02 +0200

    hw_28 fix

--- js/script.js
+++ js/script.js
@@ -5,11 +5,7 @@ const degree = prompt('Введіть ступінь, до якого ' +
   'це число необхідно піднести !');
 
 function mathDegree(one, two = 1) {
-  if (isNaN(one) || isNaN(two)) {
-    return 'помилка';
-  } else {
-    return Math.pow(one, two);
-  }
+  return isNaN(one) || isNaN(two) ?  'помилка' :  Math.pow(one, two);
 }
 
 const result = mathDegree(num, degree);
