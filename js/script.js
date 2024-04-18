c939c49b35ac209055c24b50c4ecd82fd083561a
Oleksandr <aleksandr.master88@gmail.com>
2024-04-18 05:00:21 +0300

    hw_55 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this
+    },
+    down() {
+        this.step--;
+        return this
+    },
+    showStep() {
+        return this
+    }
+
+};
+
+console.log(ladder.up().up().down().showStep())
+
+
