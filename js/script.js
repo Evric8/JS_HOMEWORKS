5b70a6d39e88f16cfec243c6d0690451206b1c4f
Oleksandr <aleksandr.master88@gmail.com>
2024-04-22 07:51:40 +0300

    hw_56 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const obj = {
+    age: 42,
+    aa: 1,
+    funcStart( ...arg) {
+        return this
+    },
+};
+
+const obj2 = {
+    age : 45,
+    aa: 45
+}
+
+const myApply = function(func, context, arr){
+    const key = Date.now().toString()
+    context.key = func
+    const result = context.key(arr)
+    delete context.key
+    return result
+}
+console.log(myApply(obj.funcStart, obj2,[]))
+
