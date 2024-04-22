dd4f65696c1811c95c355b0e7b2b4a28cea48696
Oleksandr <aleksandr.master88@gmail.com>
2024-04-22 07:33:14 +0300

    hw_54 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-'use strict';
+
+const obj = {
+    age: 42,
+    aa: 1,
+    funcStart( ...arg) {
+        return this
+    },
+};
+
+const obj2 = {
+    age : 45,
+    aa: 45
+}
+
+function myBind2 (fn, context, ...args){
+
+    return function(...argsFuncStart){
+        const key = Date.now().toString()
+        context[key] = fn
+        const result = context[key](...args.concat(argsFuncStart))
+        delete context[key]
+        return result
+    }
+}
+
+console.log(myBind2(obj.funcStart, obj2)())
 
