a9799fb3a5d62c69a2fbede967a5a2917eaa98ba
Oleksandr <aleksandr.master88@gmail.com>
2024-04-24 00:47:58 +0300

    hw_57 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+function funClose(sum){
+    return function (b) {
+        sum += b
+        return `sum(${b}) = ${sum}`
+    }
+}
+
+const sum = funClose(0)
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+
+
+
+
