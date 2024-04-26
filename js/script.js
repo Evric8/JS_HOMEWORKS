db60291fe7bc355c77367c63c923c075f916c265
Oleksandr <aleksandr.master88@gmail.com>
2024-04-26 15:24:09 +0300

    hw_59 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+(function(){
+
+    const ul = document.getElementById('ulId');
+    const childElementsText = []
+
+    for(let child of ul.childNodes){
+        if(child.nodeType === 1){
+            console.log(child);
+            childElementsText.push(child.innerHTML)
+        }
+    }
+
+    console.log(childElementsText.length);
+    console.log(childElementsText);
+
+})()
