a98a091978d5845eee1bcfd3ae0a39ce0252d8b4
Oleksandr <aleksandr.master88@gmail.com>
2024-04-27 15:34:17 +0300

    hw_60 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+(function(){
+    const ul = document.getElementById('ulId');
+    const attributesUl = []
+    const attributesNameUl = []
+
+    for(let attr of ul.attributes){
+        attributesUl.push(attr.nodeValue)
+        attributesNameUl.push(attr)
+    }
+    console.log(attributesUl);
+    console.log(attributesNameUl);
+
+
+    ul.lastElementChild.innerHTML = 'Привіт мене звати Олександр'
+    console.log(ul.lastElementChild.innerText);
+
+
+    ul.firstElementChild.setAttribute('data-my-name',
+        'Oleksandr')
+    console.log(ul.firstElementChild);
+
+
+    ul.removeAttribute('data-dog-tail')
+    console.log(ul.attributes);
+})()
+
