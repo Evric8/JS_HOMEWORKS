ac2decbff123b8efea240ad0be7b284f8e7b20ce
Oleksandr <aleksandr.master88@gmail.com>
2024-05-12 19:57:01 +0300

    hw_62 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-'use strict';
+(function(){
+    const list = function generateList(array) {
+        const result = array.reduce((ul, item) => {
+            const li = document.createElement('li')
 
+            if (!Array.isArray(item)) {
+                li.textContent = item
+            } else {
+                const innerUl = list(item)
+                li.appendChild(innerUl)
+            }
+
+            ul.appendChild(li)
+            return ul
+
+        }, document.createElement('ul'))
+
+        return document.body.appendChild(result)
+    }
+
+    console.log(list([1,2, [1.1,1.2,1.3] ,3]))
+})()
