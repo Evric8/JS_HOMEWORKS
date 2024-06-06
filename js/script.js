877536a10e926dea931600185387952c990645c4
Oleksandr <aleksandr.master88@gmail.com>
2024-06-06 08:51:01 +0300

    hw_68 commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const arr = [11, 3];
+const myIteral = (arr) => {
+    const iterals = Array.from(arr);
+    let current = 0;
+    return {
+        next() {
+            let done = false;
+            if (current + 1 > iterals.length) {
+                done = true;
+            }
+            const result = {
+                value: iterals[current],
+                done,
+            };
+            current += 1;
+            return result;
+        },
+    };
+};
+
+const iteral = myIteral(arr);
+console.log(iteral);
+console.log(iteral.next());
+console.log(iteral.next());
+console.log(iteral.next());
+
